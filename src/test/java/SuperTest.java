27cb1d00a529cfffd839159d4a51d53a68a3c27c
Leroy Omondi <leroyomondi@gmail.com>
2017-08-17 14:34:33 +0300

    add first test

--- src/test/java/SuperTest.java
+++ src/test/java/SuperTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SuperTest {
-
+    @Test
+    public void SuperHero_instantiatesCorrectly_true() {
+        Super superHero = new Super("Superman");
+        assertEquals(true, superHero instanceof Super);
+    }
 }
